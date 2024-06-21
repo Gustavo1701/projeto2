99b09d73e37b84d3f42665b86ec57635f51e0efb
~~ <~~>
2024-06-20 21:30:45 -0300

    commit do Ed

--- js/index.js
+++ js/index.js
@@ -8,3 +8,10 @@ function mostrarDataHora(){
     
 }
 mostrarDataHora();
+
+function alterarNomeAlteracao() {
+        const nomeAlteracao = document.getElementById('nomeAlteracao');
+        nomeAlteracao.innerText = nomeAlteracao.innerText.toUpperCase();
+}
+
+alterarNomeAlteracao();
